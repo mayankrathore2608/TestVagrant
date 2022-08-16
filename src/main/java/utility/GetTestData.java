88e9c2d9ef495dcc9fb0606e41e4719ba4eb3b2c
Mayank Rathore <mayank.rathore@sirionlabs.local>
2022-08-16 23:24:43 +0530

    created class which brings json string to test class

--- src/main/java/utility/GetTestData.java
+++ src/main/java/utility/GetTestData.java
@@ -1,11 +1,10 @@
 package utility;
 
+import resouces.TestData;
 public class GetTestData {
 
-
-
-    
-
-
+    public static String getJsonString(){
+        return  TestData.JSON_STRING;
+    }
 
 }
