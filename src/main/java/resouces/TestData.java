7fec2bdab412d5eba63511a6943aa26eb41a36de
Mayank Rathore <mayank.rathore@sirionlabs.local>
2022-08-16 23:21:40 +0530

    Added json

---
+++ src/main/java/resouces/TestData.java
@@ -0,0 +1,77 @@
+package resouces;
+
+public class TestData {
+   public static String JSON_STRING = "  \"{\\n\" +\n" +
+            "                \"  \\\"name\\\": \\\"Royal Challengers Bangalore\\\",\\n\" +\n" +
+            "                \"  \\\"location\\\": \\\"Bangalore\\\",\\n\" +\n" +
+            "                \"  \\\"player\\\": [\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Faf Du Plessis\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"South Africa\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Batsman\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"7\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Virat Kohli\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Batsman\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"15\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Glenn Maxwell\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"Australia\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Batsman\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"11\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Mohammad Siraj\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Bowler\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"7\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Harshal Patel\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Bowler\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"10.75\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Wanindu Hasaranga\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"Srilanka\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Bowler\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"10.75\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Dinesh Karthik\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Wicket-keeper\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"5.5\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Shahbaz Ahmed\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"All-Rounder\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"2.4\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Rajat Patidar\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Batsman\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"0.20\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Josh Hazlewood\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"Australia\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Bowler\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"7.75\\\"\\n\" +\n" +
+            "                \"    },\\n\" +\n" +
+            "                \"    {\\n\" +\n" +
+            "                \"      \\\"name\\\": \\\"Mahipal Lomror\\\",\\n\" +\n" +
+            "                \"      \\\"country\\\": \\\"India\\\",\\n\" +\n" +
+            "                \"      \\\"role\\\": \\\"Bowler\\\",\\n\" +\n" +
+            "                \"      \\\"price-in-crores\\\": \\\"7.75\\\"\\n\" +\n" +
+            "                \"    }\\n\" +\n" +
+            "                \"  ]\\n\" +\n" +
+            "                \"}\" ";
+
+}
---
+++ src/main/java/utility/GetTestData.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class GetTestData {
+
+
+
+    
+
+
+
+}
